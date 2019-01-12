05ddf7183e09973da6b149773f3213038e296365
Estefano Correa <esteffbh@gmail.com>
2019-01-11 22:09:44 -0200

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -4,41 +4,43 @@ import boardgame.Position;
 
 public class ChessPosition 
 {
+	
 	private char column;
 	private int row;
 	
 	public ChessPosition(char column, int row) 
 	{
-		if (column < 'a'|| column > 'h' || row < 1 || row > 8)
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) 
 		{
-			throw new ChessException("Error instantiating ChessPosition. Valid values are from (a,1) to (h,8)");
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
 		}
 		this.column = column;
 		this.row = row;
 	}
 
-	public char getColumn() {
+	public char getColumn() 
+	{
 		return column;
 	}
 
-	public int getRow() {
+	public int getRow() 
+	{
 		return row;
 	}
-	
-	protected Position toPosition()
+
+	protected Position toPosition() 
 	{
 		return new Position(8 - row, column - 'a');
 	}
 	
-	protected static ChessPosition fromPosition(Position position)
+	protected static ChessPosition fromPosition(Position position) 
 	{
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
-	public String toString()
+	public String toString() 
 	{
-		//"" usado para mostrar ao compilador que a sequencia é uma concatenação de strings
-		return "" + column + row; 
+		return "" + column + row;
 	}
 }
